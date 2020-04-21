ea0f7916ef003affbff6163049e3bdd1d27ef132
Vignesh Shetty <61915786+vigneshshettyalike@users.noreply.github.com>
2020-04-21 11:07:40 +0530

    Add files via upload

---
+++ SDL_STARTER_KIT/_16_FlameSensor/_16_FlameSensor/_16_FlameSensor.ino
@@ -0,0 +1,20 @@
+/*********************************************************** 
+File name:   16_flamesensor.ino 
+Description:  Flame Sensor. 
+Website: www.sdlsmartlabs.com
+ ***********************************************************/ 
+
+int flameSensorPin = 0; // the cell and 10K pulldown are connected to a0 
+int flameSensorReading; // the analog reading from the analog resistor divider 
+ 
+void setup(void) 
+{  Serial.begin(9600);  } 
+ 
+void loop(void) { 
+ 
+flameSensorReading = analogRead(flameSensorPin);  
+ 
+Serial.print("Analog reading = "); 
+Serial.println(flameSensorReading); // the raw analog reading delay(1000); 
+ 
+}
---
+++ SDL_STARTER_KIT/_17_ActiveBuzzer/_17_ActiveBuzzer/_17_ActiveBuzzer.ino
@@ -0,0 +1,22 @@
+/*********************************************************** 
+File name:   _17_aBuzzer.ino 
+Description:  Active Buzzer. 
+Website: www.sdlsmartlabs.com
+***********************************************************/ 
+
+int buzzerPin = 9; 
+ 
+void setup () 
+{   
+  pinMode (buzzerPin, OUTPUT); 
+} 
+ 
+void loop ()
+{   
+digitalWrite (buzzerPin, HIGH);  
+delay (500);   
+digitalWrite (buzzerPin, LOW);   
+delay (500); 
+} 
+ 
+ 
